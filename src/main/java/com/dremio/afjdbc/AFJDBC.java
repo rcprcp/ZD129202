4d32f82a2b93877e98da1e09dfc2cd3216a62202
Bob Plotts <bob.plotts@dremio.com>
2023-12-15 10:55:46 -0500

    Formatting changes

--- src/main/java/com/dremio/afjdbc/AFJDBC.java
+++ src/main/java/com/dremio/afjdbc/AFJDBC.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 Dremio Corporation
+ * Copyright (C) 2023 Dremio Corporation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,6 +27,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Types;
 import java.util.Collections;
+import java.util.Random;
 import java.util.Scanner;
 
 public class AFJDBC {
@@ -68,96 +69,77 @@ public class AFJDBC {
 
       Statement stmt = conn.createStatement();
 
-//      String dataSource = "Samples.\"samples.dremio.com\".\"NYC-taxi-trips\"";
-//      String dataSource = "Samples.\"samples.dremio.com\".\"tpcds_sf1000\".\"catalog_sales\"";
-        String dataSource = "\"rajan-sample-databucket\".\"case125370_156Cols_20M\"";
+      String dataSource = "\"bob-data\".besttable";
+      while (true) {
+        final String sql = String.format("SELECT * FROM %s WHERE ID = %d",
+                                         dataSource,
+                                         new Random().ints(1, 5000).findFirst().getAsInt());
 
-      final String sql = String.format("SELECT * FROM %s", dataSource);
+        int recordCount = 0;
+        long startTime = System.currentTimeMillis();
 
-      // TODO: do we need this additioal information?  Does it help with testing?
-      //  String cat = conn.getCatalog();
-      //  DatabaseMetaData dbmd = conn.getMetaData();
+        try (ResultSet rs = stmt.executeQuery(sql)) {
+          ResultSetMetaData metaData = rs.getMetaData();
+          int columnCount = metaData.getColumnCount();
+          System.out.println(sql);
+          while (rs.next()) {
+            // although it's not much processing, let's touch all the columns.
+            for (int i = 1; i <= columnCount; i++) {
+              switch (metaData.getColumnType(i)) {
+                case Types.DOUBLE:
+                case Types.REAL:
+                  Double d = rs.getDouble(i);
+                  continue;
 
-      int recordCount = 0;
-      long startTime = System.currentTimeMillis();
+                case Types.BIGINT:
+                case Types.INTEGER:
+                  Long ll = rs.getLong(1);
+                  continue;
 
-      try (ResultSet rs = stmt.executeQuery(sql)) {
-        ResultSetMetaData metaData = rs.getMetaData();
-        int columnCount = metaData.getColumnCount();
+                case Types.DECIMAL:
+                  BigDecimal bd = rs.getBigDecimal(i);
+                  continue;
 
-        while (rs.next()) {
-          // although it's not much processing, let's touch all the columns.
-          for (int i = 1; i <= columnCount; i++) {
-            switch (metaData.getColumnType(i)) {
-              case Types.DOUBLE:
-              case Types.REAL:
-                Double d = rs.getDouble(i);
-                continue;
+                case Types.NVARCHAR:
+                case Types.CHAR:
+                case Types.VARCHAR:
+                  String str = rs.getString(i);
+                  continue;
 
-              case Types.BIGINT:
-              case Types.INTEGER:
-                Long ll = rs.getLong(1);
-                continue;
+                case Types.DATE:
+                  java.sql.Date dt = rs.getDate(i);
+                  continue;
 
-              case Types.DECIMAL:
-                BigDecimal bd = rs.getBigDecimal(i);
-                continue;
+                case Types.TIME:
+                  java.sql.Time t = rs.getTime(i);
+                  continue;
 
-              case Types.NVARCHAR:
-              case Types.CHAR:
-              case Types.VARCHAR:
-                String str = rs.getString(i);
-                continue;
+                case Types.TIMESTAMP:
+                  java.sql.Timestamp ts = rs.getTimestamp(i);
+                  continue;
 
-              case Types.DATE:
-                java.sql.Date dt = rs.getDate(i);
-                continue;
+                default:
+                  String s = rs.getString(i);
+                  //TODO:  This may print a lot, should we limit it?
+                  System.out.println("unhandled type: " + metaData.getColumnType(i));
+              }
+            }
 
-              case Types.TIME:
-                java.sql.Time t = rs.getTime(i);
-                continue;
-
-              case Types.TIMESTAMP:
-                java.sql.Timestamp ts = rs.getTimestamp(i);
-                continue;
-
-              default:
-                String s = rs.getString(i);
-                //TODO:  This may print a lot, should we limit it?
-                System.out.println("unhandled type: " + metaData.getColumnType(i));
+            recordCount++;
+            if (recordCount % 30000 == 0) {
+              LOG.info("{} records", recordCount);
             }
           }
+          rs.close();
 
-          recordCount++;
-          if (recordCount % 30000 == 0) {
-            LOG.info("{} records", recordCount);
-          }
+        } catch (SQLException ex) {
+          LOG.error("SQLException: {}", ex.getMessage(), ex);
+          System.exit(5);
         }
-        rs.close();
-        stmt.close();
-
-      } catch (SQLException ex) {
-        LOG.error("SQLException: {}", ex.getMessage(), ex);
-        System.exit(5);
       }
-
-      long elapsed = (System.currentTimeMillis() - startTime)/1000;
-      long rps = recordCount / elapsed;
-      LOG.info("read {} records in {} seconds. {} records per second", recordCount, elapsed, rps);
-
     } catch (SQLException ex) {
       LOG.error("SQLException: {}", ex.getMessage(), ex);
       System.exit(5);
-    }
-  }
-
-  void napTime(long ms) {
-    try {
-      Thread.sleep(ms);
-    } catch (InterruptedException ex) {
-      System.out.println("InterruptedException: " + ex.getMessage());
-      ex.printStackTrace();
-      System.exit(3);
     }
   }
 }
